43da81a7c51b3e1d2f26f078bb2197b775357edf
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-04-18 21:47:11 -0500

    se organiza los modelos en una carpeta de Models

--- app/Http/Controllers/AddSoportesEstudiosController.php
+++ app/Http/Controllers/AddSoportesEstudiosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\SoporteEstudioModel;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
