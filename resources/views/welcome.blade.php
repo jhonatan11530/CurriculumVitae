b3504bc14915f4a625be20fb8e11f247280db3af
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-05-18 21:02:24 -0500

    se mejora inicio de la presentacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,14 @@
 @extends('Layout/plantilla')
 @section('contenido')
-asd
+<div class="page-inner mt--5">
+    <div class="row mt--2">
+        <div class="col-md-12">
+            <div class="card full-height">
+                <div class="card-body text-center">
+                    <h2>Bienvenido a la aplicacion web para crear hoja de vida</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
